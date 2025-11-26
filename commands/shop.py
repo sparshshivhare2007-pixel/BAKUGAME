7c0c3628ec96c28fc439f54ab5d26e232c5d5a20
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-11-26 21:15:37 +0530

    Update shop command to display new items

--- commands/shop.py
+++ commands/shop.py
@@ -1,7 +1,8 @@
-def shop_cmd(update, context):
-    items = [
-        "Laptop - 5000 coins",
-        "Bike - 20000 coins",
-        "Car - 50000 coins"
-    ]
-    update.message.reply_text("🛒 Shop:\n" + "\n".join(items))
+from telegram import Update
+from telegram.ext import CallbackContext
+
+def shop_cmd(update: Update, context: CallbackContext):
+    # Shop items yahan show karenge
+    items = ["Sword - 50 coins", "Shield - 40 coins", "Potion - 10 coins"]
+    shop_text = "🛒 Shop Items:\n" + "\n".join(items)
+    update.message.reply_text(shop_text)
