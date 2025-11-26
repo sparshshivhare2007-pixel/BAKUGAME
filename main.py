80a7e1e67d29f2249eed520cad0f188b06f0e92e
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-11-26 19:49:58 +0530

    𝑺𝒉𝒊𝒛𝒖𝒌𝒂𝒃𝒐𝒕𝒔

---
+++ main.py
@@ -0,0 +1,33 @@
+from telegram.ext import Updater, CommandHandler
+import configparser
+from database import create_user
+from commands.work import work_cmd
+from commands.daily import daily_cmd
+from commands.profile import profile_cmd
+from commands.rob import rob_cmd
+from commands.shop import shop_cmd
+from commands.buy import buy_cmd
+from commands.leaderboard import leaderboard_cmd
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+TOKEN = config["bot"]["token"]
+
+updater = Updater(TOKEN, use_context=True)
+dp = updater.dispatcher
+
+def start(update, context):
+    create_user(update.effective_user.id)
+    update.message.reply_text("🤖 Economy Bot Online!\nUse /work /daily /profile /rob /shop /buy /leaderboard")
+
+dp.add_handler(CommandHandler("start", start))
+dp.add_handler(CommandHandler("work", work_cmd))
+dp.add_handler(CommandHandler("daily", daily_cmd))
+dp.add_handler(CommandHandler("profile", profile_cmd))
+dp.add_handler(CommandHandler("rob", rob_cmd))
+dp.add_handler(CommandHandler("shop", shop_cmd))
+dp.add_handler(CommandHandler("buy", buy_cmd))
+dp.add_handler(CommandHandler("leaderboard", leaderboard_cmd))
+
+updater.start_polling()
+updater.idle()
