32055a0736c7bf067f32d04e64b0b0e914d1ab94
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-11-26 21:15:18 +0530

    Update rob.py

--- commands/rob.py
+++ commands/rob.py
@@ -1,13 +1,7 @@
-import random
-from database import get_user, update_user, create_user
+from telegram import Update
+from telegram.ext import CallbackContext
 
-def rob_cmd(update, context):
+def rob_cmd(update: Update, context: CallbackContext):
     user_id = update.effective_user.id
-    create_user(user_id)
-
-    if not context.args:
-        update.message.reply_text("Usage: /rob <username>")
-        return
-    
-    victim_name = context.args[0]
-    update.message.reply_text("Robbing is not fully implemented yet!")
+    # Rob logic yahan
+    update.message.reply_text(f"🏴 User {user_id} tried to rob someone but failed!")
